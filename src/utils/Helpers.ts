98b8d40ca464c2184e0cf8e38a132b0098cab95a
kornjiradej <kornjiradej@gmail.com>
2024-08-14 15:31:57 +0700

    fix: product env

--- src/utils/Helpers.ts
+++ src/utils/Helpers.ts
@@ -1,6 +1,6 @@
 export const getBaseUrl = () => {
   if (process.env.NODE_ENV === 'production') {
-    return `https://${process.env.NEXT_PUBLIC_URL}`;
+    return process.env.NEXT_PUBLIC_URL;
   }
 
   if (process.env.NEXT_PUBLIC_URL) {
